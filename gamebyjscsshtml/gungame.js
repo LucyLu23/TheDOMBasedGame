29d49cad28f29bd778d89104bddd5122f5dabc6d
LucyLu23 <114943897+LucyLu23@users.noreply.github.com>
2022-12-10 15:19:37 -0700

    createloginpage

--- gamebyjscsshtml/gungame.js
+++ gamebyjscsshtml/gungame.js
@@ -1,13 +1,18 @@
 
 var button1 = document.getElementById('StartGame');
 button1.addEventListener('click',func1);
+var div1= document.getElementById('div1');
 function func1(element){
     element.innerText = 'the Gun-fighting Game begins';
     //element.style.webkitAnimationPlayState = "running";
     //element.style.color = "blue";
-    alert('Welcome to the Gun-fighting Game') 
-        
+    //alert('Welcome to the Gun-fighting Game') 
+    //document.getElementById('StartGame').innerHTML="Welcome to the Gun-fighting Game";
+    //var div1= document.getElementById('div1');
+    //div1.innerText =`Hi, ${PlayerName} Welcome to the Gun-fighting Game`;
+    div1.innerText =`Hi, ${PlayerName} Welcome to the Gun-fighting Game`;
 }
+
 
 /* button2.addEventListener('click',function(){
     //alert('nihao');
@@ -38,22 +43,21 @@ class Player {
         this.name = name;
         this.bullets = bullets;
         this.accuracy = accuracy;
-       
         this.scores = scores;
         
     }
     //accuracy2=Math.random();
     greet(greetingwords){
-        alert(greetingwords +'!')
+        alert(greetingwords +'!');
     }
     start(){
         this.fighting = true;
-        alert('fighting...')
+        alert('fighting...');
     }
 
 }
-var Player11 = new Player(button1, "Jack", 6, 0.7, 0);
-var Player22 = new Player(button2, "Jacob", 6, 0.7, 0 );
+var Player11 = new Player(button1, "Jack", 5, 0.7, 0);
+var Player22 = new Player(button2, "Jacob", 5, 0.7, 0 );
 
 /* 
 function openwin(){
@@ -100,23 +104,23 @@ console.log(Player11.accuracy1); */
 var button2 = document.getElementById('Player1');
 var button3 = document.getElementById('Player2'); 
 
-for (let i = 1; i <= Player11.bullets; i++){
 
+for (var i = 1; i <= Player11.bullets; i++){
     function func2(element){
         element.innerText = 'Player1 completed';
-        //element.style.color = "blue";  
-        Player1HitRate = Math.random(); 
+        Player1HitRate = (Math.random()).toFixed(2); 
         console.log(Player1HitRate);
         if (Player1HitRate >= Player11.accuracy){
-            Player11.scores = Player11.scores + 10;
-            
-            alert(`Player1 succeeded the ${i} round, Player1 Scores is ${Player11.scores}`)
-            Player11.greet('Yah!')
-        
+            Player11.scores =+ 10;
+            div1.innerText =`Yah!  Player1 succeeded the ${i}th round, Player1 Scores are ${Player11.scores}`;
+            //alert(`Player1 succeeded the ${i} round, Player1 Scores is ${Player11.scores}`)
+            //Player11.greet('Yah!')
         }else{
             Player11.scores = Player11.scores + 0;
-            alert(`Player1 missed the ${i} round, Player1 Scores is ${Player11.scores}`)
-            Player11.greet('Gosh!')
+            //var div1= document.getElementById('div1');
+            div1.innerText =`Gosh! Player1 missed the ${i}th round, Player1 Scores are ${Player11.scores}`;
+           // alert(`Player1 missed the ${i} round, Player1 Scores is ${Player11.scores}`)
+            //Player11.greet('')
         }
         var Player1Total = Player11.scores;
         console.log(Player1Total);
@@ -130,17 +134,16 @@ for (let i = 1; i <= Player11.bullets; i++){
       
     function func3(element){
         element.innerText = 'Player2 completed';
-        //element.style.color = "yellow";
-        Player2HitRate = Math.random(); 
+        Player2HitRate = (Math.random()).toFixed(2); 
         console.log(Player2HitRate);
         if (Player2HitRate >= Player22.accuracy){
-            Player22.scores = Player22.scores + 10;
-            alert(`Player2 succeeded the ${i}  round, Player2 Scores is ${Player22.scores}`)
-            Player22.greet('Wow!');
+            Player22.scores =+ 10;
+            div1.innerText =`'Wow!' Player2 succeeded the ${i}th  round, Player2 Scores are ${Player22.scores}`;
+            
         }else{
-            Player22.scores = Player22.scores + 0;
-            alert(`Player2 missed the  ${i}  round, Player2 Scores is ${Player22.scores}`)
-            Player22.greet('What?!');
+            Player22.scores =+ 0;
+            div1.innerText =`'What?!' Player2 missed the  ${i}th  round, Player2 Scores are ${Player22.scores}`;
+           
         }
         var Player2Total = Player22.scores;
         console.log(Player2Total);
@@ -154,58 +157,32 @@ for (let i = 1; i <= Player11.bullets; i++){
         
     }
     button3.addEventListener('click', func3); 
-    func4(Player11.scores,Player22.scores);     
-
-
+    //func4(Player11.scores,Player22.scores);   
+    
+   
 }
 
-
-   
 var button4 = document.getElementById('CheckResult');
-function func4(Player1Total,Player2Total  ) {
-    if (Player1Total > Player2Total){
-         alert(`${Player11.name} is the winner`)
-    }else if (func2.call(Player1Total) =  func3.call(Player2Total)){
-        alert('Nobody wins, restart the game')
-    func1(); 
-  
+function func4() {
+if (Player11.scores> Player22.scores){
+    div1.innerText =`${Player11.name} is the winner`;
+}else if (Player11.scores = Player22.scores){
+    div1.innerText ='Nobody wins, restart the game';
+func1(); 
+
 }else {
-    alert(`${Player22.name} is the winner`)
+    div1.innerText =`${Player22.name} is the winner`;
 }
 }  
 button4.addEventListener('click',func4);  
-
+ 
 var button5 = document.getElementById('Restart');
 function func5(element1){
-    element1.innerText = 'Restart the Gun-fighting Game';
+    element1.innerText = 'End the Gun-fighting Game';
     element1.style.color = "red";
   
-    func1(element1);
+    func1();
 }
-button5.addEventListener('click',func5); 
+button5.addEventListener('click',func5);  
 
-    /* for (let i = 1; i<=Player11.bullets; i++) {
-        prescores = Player11.scores;
-        if (Player11.accuracy2 >= Player11.accuracy1){
-            Player11.scores = prescores + 10;
-            alert(`the ${i} round, Player11 Scores is ${Player11.scores}`)
-        }else{
-            Player11.scores = prescores
-            alert(`the ${i} round, Player11 Scores is ${Player11.scores}`)
 
-        }
-        if (Player22.accuracy2 >= Player22.accuracy1){
-            Player22.scores = prescores + 10;
-            alert(`the ${i} round, Player22 Scores is ${Player22.scores}`)
-        }else{
-            Player22.scores = prescores
-            alert(`the ${i} round, Player22 Scores is ${Player22.scores}`)
-
-        }
-        
-    } */
-   
-
-/* function func3(){
-    alert('Restart the Gun-fighting Game')
-    prompt('You are awesome, and your name and age?' , '>=18?'))*/
